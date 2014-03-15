0de5ef0454d4f4b0f666bf29c894f4538e88aefa
jdeck88 <jdeck88@baa27788-af8f-33cd-b8c5-2904ab3414b2>
2014-03-15 00:46:57 +0000

    created investigation and assay outputs

--- src/exports/exportConnector.java
+++ src/exports/exportConnector.java
@@ -2,11 +2,17 @@ package exports;
 
 import utils.PathManager;
 import utils.SettingsManager;
+import utils.biocodeExportsFileOutputStream;
 import utils.database;
 
+import javax.security.auth.Subject;
 import java.awt.geom.Path2D;
 import java.io.File;
+import java.io.IOException;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 
 /**
  * Super-class for connecting to the Biocode Databzse
@@ -40,5 +46,36 @@ public class exportConnector {
                 exportDir +
                 File.separatorChar +
                 subPath);
+    }
+
+    public String writeResultSet(ResultSet rs, File file) throws IOException, SQLException {
+        biocodeExportsFileOutputStream befo = new biocodeExportsFileOutputStream(file);
+
+        // Use column names as the header
+        ResultSetMetaData rsmd = rs.getMetaData();
+        int columnCount = rsmd.getColumnCount();
+        // The column count starts from 1
+        for (int i = 1; i < columnCount + 1; i++) {
+            if (i > 1) befo.write("\t");
+            befo.write(rsmd.getColumnLabel(i));
+        }
+        befo.write("\n");
+
+        // Loop through the contents
+        while (rs.next()) {
+            for (int i = 1; i < columnCount + 1; i++) {
+                if (i > 1) befo.write("\t");
+                try {
+                    befo.write(rs.getString(i));
+                } catch (Exception e) {
+                    befo.write("");
+                }
+            }
+            befo.write("\n");
+        }
+        befo.close();
+
+        return file.getAbsoluteFile().toString();
+
     }
 }
