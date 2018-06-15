686debfd18ed41cb209be14cec4a0a4407295f3a
John Deck <jdeck88@gmail.com>
2018-06-15 14:06:36 -0700

    renaming some convenience scripts to match java class names they call...easier this way to remember

---
+++ exportIndoForDwC.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+/usr/bin/java -cp /home/jdeck/code/biocode-exports/lib/*:/home/jdeck/code/biocode-exports/:/home/jdeck/code/biocode-exports/out/production/biocode-exports exports.exportAsDarwinCore -o /data/ipt/data/resources/biocode/dwc/indo -p INDO
+
+
