5d5377aca7f15669f56c177f3a01597abd91df18
John Deck <jdeck88@gmail.com>
2019-03-22 13:29:34 -0700

    adding photos directory with results of biocode photos from calphotos and the photomatch table

---
+++ photos/findID_photos.sh
@@ -0,0 +1,3 @@
+# ID_folders on louise
+# i'm not sure what to do with these photos, many not integrated into current site
+find /data/biocode/photos/ID_folders/ -maxdepth 2 -iname '*.jpg' -o -iname '*.jpeg'
